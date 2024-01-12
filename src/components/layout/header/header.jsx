bc0225ea6c7aeaf64c090c46edcae9c3ac158188
Oscar_Gutierrez <oscarfgutierrezo@gmail.com>
2024-01-12 12:04:59 -0500

    Remove: useeffect en header component

--- src/components/layout/header/header.jsx
+++ src/components/layout/header/header.jsx
@@ -1,6 +1,4 @@
-import { useEffect, useState } from "react";
-import { LocationIcon, PhoneIcon } from "../../icons";
-import { InfoLink } from "./infoLink";
+import { useState } from "react";
 import { Navbar } from "./navbar/navbar";
 import { HamburgerBtn } from "./hamburgerBtn";
 import { Link } from "react-router-dom";
@@ -9,12 +7,6 @@ import { NavbarFull } from "./navbar/navbarFull";
 
 export const Header = () => {
   const [menuIsOpen, setMenuIsOpen] = useState(false);
-
-  useEffect(() => {
-    if (window.innerWidth >= 1024) {
-      setMenuIsOpen(true);
-    }
-  }, []);
 
   return (
     <header className="relative p-5 flex justify-between items-center xs:px-7 sm:px-10">
@@ -30,27 +22,6 @@ export const Header = () => {
           <NavbarFull />
         </div>
       </div>
-      {/* <div className="flex flex-col justify-between gap-3">
-        <div className="flex flex-col items-end md:flex-row-reverse md:items-start md:gap-5">
-          <InfoLink
-            path="#"
-            icon={<LocationIcon width={20} height={25} />}
-            text="2500 NW 39th Street Miami, Florida 33142"
-          />
-          <InfoLink
-            path="#"
-            icon={<PhoneIcon width={24} height={30} />}
-            text="786.877.4317"
-          />
-        </div>
-        <div className="flex justify-end lg:hidden">
-          <HamburgerBtn menuIsOpen={menuIsOpen} setMenuIsOpen={setMenuIsOpen} />
-          <Navbar menuIsOpen={menuIsOpen} />
-        </div>
-        <div className="hidden lg:block">
-          <NavbarFull />
-        </div>
-      </div> */}
     </header>
   );
 };
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import { Layout } from "../components/layout/layout";
 import { BannerHome } from "../components/pages/banner-home";
 import { CardHome } from "../components/pages/card-home";
@@ -18,22 +17,6 @@ export const Home = () => {
             link="/rentals"
           />
         </section>
-        {/* <section className="px-5 py-14 bg-gainsboro sm:px-10">
-          <h2 className="pb-5 text-4xl uppercase">About Us</h2>
-          <p className="pb-5 leading-8 text-[#1c1c1c]">
-            MVP Exotics is Miami’s premiere luxury rental platform. MVP is known
-            as the celebrities’ favorite in Miami. We cater to all of our
-            client’s unique needs, and we treat them all with our exclusive MVP
-            treatment.
-          </p>
-          <Link
-            to="/about-us"
-            className="w-max px-5 py-1 text-base
-             font-seravekmedium uppercase bg-orange rounded-full shadow-black/70 shadow-md hover:shadow-none transition-all duration-500 sm:py-2"
-          >
-            Read More
-          </Link>
-        </section> */}
         <section className="px-5 py-10 flex flex-row flex-wrap justify-center gap-10 md:flex-row bg-gainsboro">
           <CardHome
             link="/mvp-yachts"
